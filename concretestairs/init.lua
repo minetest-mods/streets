18e226bae5edc44080526a608a8b4de83d0890ec
Christian Danscheid <Christian_D_97@gmx.de>
2013-08-16 18:46:55 +0200

    nothing special

--- concretestairs/init.lua
+++ concretestairs/init.lua
@@ -2,8 +2,9 @@
 	StreetsMod: Concrete stairs (compatible to circular saw)
 ]]
 if streets.extendedBy.moreblocks == true and streets.extendedBy.prefab == false then
-	register_stair_slab_panel_micro("streets", "concrete", "streets:concrete", {cracky=2}, {"streets_concrete.png"}, "Concrete", "", nil)
+	register_stair_slab_panel_micro("streets", "concrete", "streets:concrete", {cracky=2}, {"streets_concrete.png"}, "Concrete", "concrete", nil)
 	table.insert(circular_saw.known_stairs,"streets:concrete")
+	minetest.register_alias("stairs:stair_concrete","streets:stair_concrete")
 else
 	minetest.register_alias("stairs:stair_concrete","prefab:concrete_stair")
 	minetest.register_alias("stairs:slab_concrete","prefab:concrete_slab")
