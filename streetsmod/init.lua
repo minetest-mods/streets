2177d864cc6dda6f3a641ca3aabe08b6457b570a
webdesigner97 <Christian_D_97@gmx.de>
2014-04-14 17:39:09 +0200

    hotfix for #11

--- streetsmod/init.lua
+++ streetsmod/init.lua
@@ -7,7 +7,7 @@
 ]]
 -- kaeza: intllib
 	-- Boilerplate to support localized strings if intllib mod is installed.
-	local S
+	-- local S
 	if (minetest.get_modpath("intllib")) then
 	  dofile(minetest.get_modpath("intllib").."/intllib.lua")
 	  S = intllib.Getter(minetest.get_current_modname())
