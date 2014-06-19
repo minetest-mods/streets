59610279478543d186c735e7bae96b1bf98d1005
webdesigner97 <Christian_D_97@gmx.de>
2014-06-19 23:14:01 +0200

    add t-junction pole (more coming soon)

--- streetspoles/init.lua
+++ streetspoles/init.lua
@@ -129,6 +129,35 @@ minetest.register_node(":streets:bigpole_edge", {
 		}
 	}
 })
+minetest.register_node(":streets:bigpole_tjunction", {
+	description = "Pole",
+	drop = "streets:bigpole",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	tiles = {"streets_pole.png"},
+	sunlight_propagates = true,
+	groups = {cracky = 1, level = 2, bigpole = 1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.15, -0.5, -0.15, 0.15, 0.15, 0.15},
+			{-0.15, -0.15, -0.5, 0.15, 0.15, 0.5}
+		}
+	},
+	digiline = {
+		wire = {
+			rules = {
+				{x= 0, y= 0, z=-1},
+				{x= 0, y= 0, z= 1},
+				{x= 1, y= 0, z= 0},
+				{x=-1, y= 0, z= 0},
+				{x= 0, y=-1, z= 0},
+				{x= 0, y= 1, z= 0},
+			}
+		}
+	}
+})
 minetest.register_craft({
 	output = "streets:bigpole 3",
 	recipe = {
@@ -151,5 +180,13 @@ minetest.register_craft({
 		{"","",""},
 		{"streets:bigpole","streets:bigpole",""},
 		{"","streets:bigpole",""}
+	}
+})
+minetest.register_craft({
+	output = "streets:bigpole_tjunction 2",
+	recipe = {
+		{"","",""},
+		{"streets:bigpole_edge","streets:bigpole",""},
+		{"","",""}
 	}
 })
