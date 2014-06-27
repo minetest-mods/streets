5bec2c9b546262ef2c6bd666154fafed23db28f5
CraigyDavi <craig.davison3@gmail.com>
2014-06-27 22:57:17 +0100

    Also add a hotfix for asphalt slabs
    Avoiding unknown nodes of old worlds

--- streetshotfix/init.lua
+++ streetshotfix/init.lua
@@ -6,3 +6,4 @@ minetest.register_alias("stairs:stair_asphalt","streets:stair_asphalt")
 
 -- Hotfix #2; June 27, 2014; https://github.com/webdesigner97/streets/issues/25
 minetest.register_alias("streets:asphalt_stair","streets:stair_asphalt")
+minetest.register_alias("streets:asphalt_slab","streets:slab_asphalt")
