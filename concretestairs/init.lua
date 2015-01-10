bc8c6fad68dfc10654758fa4dea2a03a24ea6f97
Rainer <rdohmen@pensionmoselblick.de>
2015-01-10 17:16:32 +0100

    Update init.lua
    Please fix typo in init.lua

--- concretestairs/init.lua
+++ concretestairs/init.lua
@@ -1,7 +1,7 @@
 --[[
 	StreetsMod: Concrete stairs (compatible to circular saw)
 ]]
-if type(register_stair_slab_panel_mirco) == "function" and streets.extendedBy.prefab == false then
+if type(register_stair_slab_panel_micro) == "function" and streets.extendedBy.prefab == false then
 	register_stair_slab_panel_micro("streets", "concrete", "streets:concrete", {cracky=2}, {"streets_concrete.png"}, "Concrete", "concrete", nil)
 	table.insert(circular_saw.known_stairs,"streets:concrete")
 	minetest.register_alias("stairs:stair_concrete","streets:stair_concrete")
