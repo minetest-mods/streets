626f4362595f826efa4915c5b01fc82d02e974fc
webdesigner97 <christian.danscheid@outlook.de>
2016-01-26 15:57:34 +0100

    add some files for API test

---
+++ streets_roadmarkings/init.lua
@@ -0,0 +1,8 @@
+--[[
+  ## StreetsMod 2.0 ##
+  Submod: roadmarkings
+  Optional: true
+  Category: Roads
+]]
+
+streets.register_road_marking("line_dashed", "streets_line_dashed.png")
---
+++ streets_roadsurface/init.lua
@@ -0,0 +1,43 @@
+--[[
+  ## StreetsMod 2.0 ##
+  Submod: roadsurface
+  Optional: true
+  Category: Roads
+]]
+
+streets.register_road_surface(":streets:asphalt", {
+  description = "Asphalt",
+  tiles = {"streets_asphalt.png"},
+  groups = {cracky = 3},
+  sounds = default.node_sound_stone_defaults(),
+  craft = {
+    output = "streets:asphalt 1",
+    type = "cooking",
+		recipe 	= "default:gravel",
+		cooktime = 2
+  }
+})
+
+streets.register_road_surface(":streets:asphalt_red", {
+  description = "Colored Asphalt",
+  tiles = {"streets_asphalt_red.png"},
+  groups = {cracky = 3},
+  sounds = default.node_sound_stone_defaults(),
+  craft = {
+    output = "streets:asphalt_red 1",
+    type = "shapeless",
+    recipe = {"streets:asphalt", "dye:red"}
+  }
+})
+
+streets.register_road_surface(":streets:asphalt_yellow", {
+  description = "Colored Asphalt",
+  tiles = {"streets_asphalt_yellow.png"},
+  groups = {cracky = 3},
+  sounds = default.node_sound_stone_defaults(),
+  craft = {
+    output = "streets:asphalt_yellow 1",
+    type = "shapeless",
+    recipe = {"streets:asphalt", "dye:yellow"}
+  }
+})
