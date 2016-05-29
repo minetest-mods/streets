e6233501657874a9f7b432f1a1318b3e850f188a
Thomas--S <Thomas--S@users.noreply.github.com>
2016-05-29 16:33:32 +0200

    Improve appearance of concrete walls. Walls mod isn't needed anymore. Additionally, added group:sign to signs.

--- streets_concrete/init.lua
+++ streets_concrete/init.lua
@@ -11,4 +11,31 @@ minetest.register_node("streets:concrete", {
   sounds =  default.node_sound_stone_defaults()
 })
 
-walls.register(":walls:concrete", "Concrete Wall", "streets_concrete.png", "streets:concrete", default.node_sound_stone_defaults())
+minetest.register_node("streets:concrete_wall", {
+	description = "Concrete Wall",
+	paramtype = "light",
+	drawtype = "nodebox",
+	tiles = {"streets_concrete.png"},
+	sunlight_propagates = true,
+	groups = {cracky = 1, level = 2, wall = 1},
+	node_box = {
+		type = "connected",
+		fixed          = {{-0.3, -0.5, -0.3, 0.3, -0.4, 0.3}, {-0.1, -0.5, -0.1, 0.1, 0.5, 0.1}},
+		connect_front  = {{-0.3, -0.5, -0.5, 0.3, -0.4, 0.3}, {-0.1, -0.5, -0.5, 0.1, 0.5, 0.1}}, -- z-
+		connect_back   = {{-0.3, -0.5, -0.3, 0.3, -0.4, 0.5}, {-0.1, -0.5, -0.1, 0.1, 0.5, 0.5}}, -- z+
+		connect_left   = {{-0.5, -0.5, -0.3, 0.3, -0.4, 0.3}, {-0.5, -0.5, -0.1, 0.1, 0.5, 0.1}}, -- x-
+		connect_right  = {{-0.3, -0.5, -0.3, 0.5, -0.4, 0.3}, {-0.1, -0.5, -0.1, 0.5, 0.5, 0.1}}, -- x+
+	},
+	connects_to = {"group:wall", "group:stone", "group:wood", "group:tree", "group:sign", "group:concrete"},
+	sound = default.node_sound_stone_defaults()
+})
+
+
+minetest.register_craft({
+	output = "streets:concrete_wall 5",
+	recipe = {
+		{"", "streets:concrete", ""},
+		{"", "streets:concrete", ""},
+		{"streets:concrete", "streets:concrete", "streets:concrete"},
+	}
+})
