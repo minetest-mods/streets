a6afe247b802ba2561d1b1e9ce2c2db02bf71638
Thomas--S <Thomas--S@users.noreply.github.com>
2016-05-29 11:35:28 +0200

    Changes the streetpoles to connected nodeboxes. Connects to everything.

--- streets_poles/init.lua
+++ streets_poles/init.lua
@@ -13,11 +13,16 @@ minetest.register_node(":streets:bigpole", {
 	sunlight_propagates = true,
 	groups = {cracky = 1, level = 2, bigpole = 1},
 	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.15, -0.5, -0.15, 0.15, 0.5, 0.15}
-		}
+		type = "connected",
+		fixed          = {-0.15, -0.15, -0.15, 0.15,  0.15, 0.15},
+		connect_top    = {-0.15, -0.15, -0.15, 0.15,  0.5,  0.15}, -- y+
+		connect_bottom = {-0.15, -0.5,  -0.15, 0.15,  0.15, 0.15}, -- y-
+		connect_front  = {-0.15, -0.15, -0.5,  0.15,  0.15, 0.15}, -- z-
+		connect_back   = {-0.15, -0.15,  0.15, 0.15,  0.15, 0.5 }, -- z+
+		connect_left   = {-0.5,  -0.15, -0.15, 0.15,  0.15, 0.15}, -- x-
+		connect_right  = {-0.15, -0.15, -0.15, 0.5,   0.15, 0.15}, -- x+
 	},
+	connects_to = {"group:crumbly", "group:cracky", "group:snappy", "group:choppy", "group:fleshy", "group:explody", "group:oddly_breakable_by_hand"},
 	on_place = minetest.rotate_node,
 	digiline = {
 		wire = {
@@ -33,93 +38,12 @@ minetest.register_node(":streets:bigpole", {
 		}
 	}
 })
-minetest.register_node(":streets:bigpole_edge", {
-	description = "Pole",
-	drop = "streets:bigpole",
-	paramtype = "light",
-	paramtype2 = "facedir",
-	drawtype = "nodebox",
-	tiles = {"streets_pole.png"},
-	sunlight_propagates = true,
-	groups = {cracky = 1, level = 2, bigpole = 1},
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.15,-0.5,-0.15,0.15,0.15,0.15},
-			{-0.15,-0.15,-0.15,0.15,0.15,-0.5}
-		}
-	},
-	digiline = {
-		wire = {
-			rules = {
-				{x= 0, y= 0, z=-1},
-				{x= 0, y= 0, z= 1},
-				{x= 1, y= 0, z= 0},
-				{x=-1, y= 0, z= 0},
-				{x= 0, y=-1, z= 0},
-				{x= 0, y= 1, z= 0},
-			}
-		}
-	}
-})
-minetest.register_node(":streets:bigpole_tjunction", {
-	description = "Pole",
-	drop = "streets:bigpole",
-	paramtype = "light",
-	paramtype2 = "facedir",
-	drawtype = "nodebox",
-	tiles = {"streets_pole.png"},
-	sunlight_propagates = true,
-	groups = {cracky = 1, level = 2, bigpole = 1},
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.15, -0.5, -0.15, 0.15, 0.15, 0.15},
-			{-0.15, -0.15, -0.5, 0.15, 0.15, 0.5}
-		}
-	},
-	digiline = {
-		wire = {
-			rules = {
-				{x= 0, y= 0, z=-1},
-				{x= 0, y= 0, z= 1},
-				{x= 1, y= 0, z= 0},
-				{x=-1, y= 0, z= 0},
-				{x= 0, y=-1, z= 0},
-				{x= 0, y= 1, z= 0},
-			}
-		}
-	}
-})
+
 minetest.register_craft({
 	output = "streets:bigpole 3",
 	recipe = {
 		{"","",""},
 		{"","default:steel_ingot",""},
 		{"","default:steel_ingot",""}
-	}
-})
-minetest.register_craft({
-	output = "streets:bigpole_edge 3",
-	recipe = {
-		{"","",""},
-		{"streets:bigpole","streets:bigpole",""},
-		{"streets:bigpole","",""}
-	}
-})
-minetest.register_craft({
-	output = "streets:bigpole_edge 3",
-	recipe = {
-		{"","",""},
-		{"streets:bigpole","streets:bigpole",""},
-		{"","streets:bigpole",""}
-	}
-})
-minetest.register_craft({
-	output = "streets:bigpole_tjunction 2",
-	recipe = {
-		{"","",""},
-		{"streets:bigpole_edge","streets:bigpole",""},
-		{"","",""}
 	}
 })
