0d920239817d74aba3288a69a55e65fa3092cc91
webdesigner97 <christian.danscheid@outlook.de>
2016-06-01 10:06:07 +0200

    add manual bollard and clean up

--- streets_bollards/init.lua
+++ streets_bollards/init.lua
@@ -1,65 +1,109 @@
 --[[
-  ## StreetsMod 2.0 ##
-  Submod: bollards
-  Optional: true
+	## StreetsMod 2.0 ##
+	Submod: bollards
+	Optional: true
 ]]
 
-local function on_rightlick(pos, node, clicker, itemstack, pointed_thing)
-  if minetest.registered_nodes[minetest.get_node(pointed_thing.above).name].bollard then
-    local top = minetest.registered_nodes[minetest.get_node(pointed_thing.above).name]
-    minetest.set_node(pointed_thing.above, {name = top.bollard.counterpart})
-  end
+local function toggle_bollard(pos, node, clicker, itemstack, pointed_thing)
+	if minetest.registered_nodes[minetest.get_node(pos).name].bollard then
+		local n = minetest.get_node(pos)
+		local ndef = minetest.registered_nodes[n.name]
+		minetest.set_node(pos, {name = ndef.bollard.counterpart, param2 = n.param2})
+	end
 end
 
 minetest.register_node("streets:bollard_driver", {
-  description = "Driver for bollards",
-  tiles = {"default_steel_block.png", "streets_bollard_top.png"},
-  groups = {cracky = 1},
-  on_rightclick = on_rightlick,
-  mesecons = {
-      effector = {
-    		rules = mesecon.rules.default,
-    		action_change = function (pos, node)
-    			on_rightlick(pos, node, nil, nil, {type = "node", under = vector.add(pos, vector.new(0, -1, 0)), above = vector.add(pos, vector.new(0, 1, 0))})
-    		end
+	description = "Driver for bollards",
+	tiles = {"streets_bollard_driver_top.png", "default_steel_block.png"},
+	groups = {cracky = 1},
+	-- on_rightclick = toggle_bollard, -- Automatic bollards mustn't be toggled manually!
+	mesecons = {
+			effector = {
+				rules = mesecon.rules.default,
+				action_change = function (pos, node)
+					toggle_bollard(vector.add(pos, vector.new(0, 1, 0)), node, nil, nil, {type = "node", under = vector.add(pos, vector.new(0, -1, 0)), above = vector.add(pos, vector.new(0, 1, 0))})
+				end
 	}}
 })
 
-minetest.register_node("streets:bollard_steel_standard_up", {
-  description = "Steel bollard",
-  tiles = {"streets_bollard_top.png", "streets_bollard_top.png", "streets_bollard_side.png"},
-  groups = {cracky = 1},
-  paramtype = "light",
-  drawtype = "nodebox",
-  sunlight_propagates = true,
-  node_box = {
+minetest.register_node("streets:bollard_steel_automatic_up", {
+	description = "Steel bollard",
+	tiles = {"streets_bollard_driver_top.png", "streets_bollard_driver_top.png", "streets_bollard_automatic_side.png"},
+	groups = {cracky = 1},
+	paramtype = "light",
+	drawtype = "nodebox",
+	sunlight_propagates = true,
+	node_box = {
 		type = "fixed",
 		fixed = {
 			{-0.3125, -0.5, -0.3125, 0.3125, 0.5, 0.3125}
 		}
 	},
-  bollard = {
-    role = "up",
-    counterpart = "streets:bollard_steel_standard_down"
-  }
+	bollard = {
+		role = "up",
+		counterpart = "streets:bollard_steel_automatic_down"
+	}
 })
 
-minetest.register_node("streets:bollard_steel_standard_down", {
-  tiles = {"streets_bollard_top.png", "streets_bollard_top.png", "streets_bollard_side.png"},
-  groups = {cracky = 1},
-  drop = "streets:bollard_steel_standard_up",
-  paramtype = "light",
-  drawtype = "nodebox",
-  walkable = false,
-  sunlight_propagates = true,
-  node_box = {
+minetest.register_node("streets:bollard_steel_automatic_down", {
+	tiles = {"streets_bollard_driver_top.png", "streets_bollard_driver_top.png", "streets_bollard_automatic_side.png"},
+	groups = {cracky = 1},
+	drop = "streets:bollard_steel_automatic_up",
+	paramtype = "light",
+	drawtype = "nodebox",
+	walkable = false,
+	sunlight_propagates = true,
+	node_box = {
 		type = "fixed",
 		fixed = {
 			{-0.3125, -0.5, -0.3125, 0.3125, -0.4375, 0.3125}
 		}
 	},
-  bollard = {
-    role = "down",
-    counterpart = "streets:bollard_steel_standard_up"
-  }
+	bollard = {
+		role = "down",
+		counterpart = "streets:bollard_steel_automatic_up"
+	}
+})
+
+minetest.register_node("streets:bollard_steel_manual_up", {
+	tiles = {"streets_bollard_manual_top.png", "streets_bollard_manual_top.png", "streets_bollard_manual_side.png"},
+	groups = {cracky = 1},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	sunlight_propagates = true,
+	on_rightclick = toggle_bollard,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, -0.5, -0.125, 0.125, 0.5, 0.125}
+		}
+	},
+	bollard = {
+		role = "up",
+		counterpart = "streets:bollard_steel_manual_down"
+	}
+})
+
+minetest.register_node("streets:bollard_steel_manual_down", {
+	tiles = {"streets_bollard_manual_side_down.png", "streets_bollard_manual_side_down.png^[transformR180", "streets_bollard_manual_side_down.png^[transformR270", "streets_bollard_manual_side_down.png^[transformR90", "streets_bollard_manual_top.png",
+},
+	groups = {cracky = 1},
+	drop = "streets:bollard_steel_manual_up",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	walkable = false,
+	sunlight_propagates = true,
+	on_rightclick = toggle_bollard,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, -0.5, 0.125, 0.125, -0.25, 1.00}
+		}
+	},
+	bollard = {
+		role = "down",
+		counterpart = "streets:bollard_steel_manual_up"
+	}
 })
