b0c4b65ce7f4fb0b755d641d6ccd58bfc21f4375
Thomas--S <Thomas--S@users.noreply.github.com>
2016-06-06 01:00:20 +0200

    Add more types of street poles. (#81)

--- streets_poles/init.lua
+++ streets_poles/init.lua
@@ -5,7 +5,7 @@
 ]]
 
 minetest.register_node(":streets:bigpole", {
-	description = "Pole",
+	description = "Pole Straight",
 	paramtype = "light",
 	paramtype2 = "facedir",
 	drawtype = "nodebox",
@@ -33,9 +33,18 @@ minetest.register_node(":streets:bigpole", {
 		}
 	}
 })
-minetest.register_node(":streets:bigpole_edge", {
-	description = "Pole",
-	drop = "streets:bigpole",
+
+minetest.register_craft({
+	output = "streets:bigpole 3",
+	recipe = {
+		{"default:steel_ingot"},
+		{"default:steel_ingot"}
+	}
+})
+
+
+minetest.register_node(":streets:bigpole_short", {
+	description = "Pole Short",
 	paramtype = "light",
 	paramtype2 = "facedir",
 	drawtype = "nodebox",
@@ -45,10 +54,10 @@ minetest.register_node(":streets:bigpole_edge", {
 	node_box = {
 		type = "fixed",
 		fixed = {
-			{-0.15,-0.5,-0.15,0.15,0.15,0.15},
-			{-0.15,-0.15,-0.15,0.15,0.15,-0.5}
+			{-0.15, -0.5, -0.15, 0.15, 0.15, 0.15}
 		}
 	},
+	on_place = minetest.rotate_node,
 	digiline = {
 		wire = {
 			rules = {
@@ -62,9 +71,64 @@ minetest.register_node(":streets:bigpole_edge", {
 		}
 	}
 })
+
+minetest.register_craft({
+	output = "streets:bigpole_short 2",
+	recipe = {
+		{"streets:bigpole"}
+	}
+})
+
+
+minetest.register_node(":streets:bigpole_edge", {
+	description = "Pole Edge",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	tiles = {"streets_pole.png"},
+	sunlight_propagates = true,
+	groups = {cracky = 1, level = 2, bigpole = 1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.15, -0.5, -0.15, 0.15, 0.15, 0.15},
+			{-0.15, -0.15, -0.15, 0.15, 0.15, -0.5}
+		}
+	},
+	on_place = minetest.rotate_node,
+	digiline = {
+		wire = {
+			rules = {
+				{x= 0, y= 0, z=-1},
+				{x= 0, y= 0, z= 1},
+				{x= 1, y= 0, z= 0},
+				{x=-1, y= 0, z= 0},
+				{x= 0, y=-1, z= 0},
+				{x= 0, y= 1, z= 0},
+			}
+		}
+	}
+})
+
+minetest.register_craft({
+	output = "streets:bigpole_edge 3",
+	recipe = {
+		{"streets:bigpole","streets:bigpole"},
+		{"streets:bigpole",""}
+	}
+})
+
+minetest.register_craft({
+	output = "streets:bigpole_edge 3",
+	recipe = {
+		{"streets:bigpole","streets:bigpole"},
+		{"","streets:bigpole"}
+	}
+})
+
+
 minetest.register_node(":streets:bigpole_tjunction", {
-	description = "Pole",
-	drop = "streets:bigpole",
+	description = "Pole T-Junction",
 	paramtype = "light",
 	paramtype2 = "facedir",
 	drawtype = "nodebox",
@@ -78,6 +142,7 @@ minetest.register_node(":streets:bigpole_tjunction", {
 			{-0.15, -0.15, -0.5, 0.15, 0.15, 0.5}
 		}
 	},
+	on_place = minetest.rotate_node,
 	digiline = {
 		wire = {
 			rules = {
@@ -91,35 +156,208 @@ minetest.register_node(":streets:bigpole_tjunction", {
 		}
 	}
 })
+
 minetest.register_craft({
-	output = "streets:bigpole 3",
+	output = "streets:bigpole_tjunction",
 	recipe = {
-		{"","",""},
-		{"","default:steel_ingot",""},
-		{"","default:steel_ingot",""}
+		{"streets:bigpole_edge","streets:bigpole_short"},
 	}
 })
-minetest.register_craft({
-	output = "streets:bigpole_edge 3",
-	recipe = {
-		{"","",""},
-		{"streets:bigpole","streets:bigpole",""},
-		{"streets:bigpole","",""}
+
+
+minetest.register_node(":streets:bigpole_corner", {
+	description = "Pole Corner",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	tiles = {"streets_pole.png"},
+	sunlight_propagates = true,
+	groups = {cracky = 1, level = 2, bigpole = 1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.15, -0.5, -0.15, 0.15, 0.15, 0.15},
+			{-0.15, -0.15, -0.15, 0.15, 0.15, -0.5},
+			{-0.15, -0.15, -0.15, 0.5, 0.15, 0.15}
+		}
+	},
+	on_place = minetest.rotate_node,
+	digiline = {
+		wire = {
+			rules = {
+				{x= 0, y= 0, z=-1},
+				{x= 0, y= 0, z= 1},
+				{x= 1, y= 0, z= 0},
+				{x=-1, y= 0, z= 0},
+				{x= 0, y=-1, z= 0},
+				{x= 0, y= 1, z= 0},
+			}
+		}
 	}
 })
+
 minetest.register_craft({
-	output = "streets:bigpole_edge 3",
+	output = "streets:bigpole_corner",
 	recipe = {
-		{"","",""},
-		{"streets:bigpole","streets:bigpole",""},
-		{"","streets:bigpole",""}
+		{"streets:bigpole_edge"},
+		{"streets:bigpole_short"}
 	}
 })
+
+
+minetest.register_node(":streets:bigpole_four_side_junction", {
+	description = "Pole 4-Side-Junction",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	tiles = {"streets_pole.png"},
+	sunlight_propagates = true,
+	groups = {cracky = 1, level = 2, bigpole = 1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.15, -0.5, -0.15, 0.15, 0.5, 0.15},
+			{-0.15, -0.15, -0.15, 0.15, 0.15, -0.5},
+			{-0.15, -0.15, -0.15, 0.5, 0.15, 0.15}
+		}
+	},
+	on_place = minetest.rotate_node,
+	digiline = {
+		wire = {
+			rules = {
+				{x= 0, y= 0, z=-1},
+				{x= 0, y= 0, z= 1},
+				{x= 1, y= 0, z= 0},
+				{x=-1, y= 0, z= 0},
+				{x= 0, y=-1, z= 0},
+				{x= 0, y= 1, z= 0},
+			}
+		}
+	}
+})
+
 minetest.register_craft({
-	output = "streets:bigpole_tjunction 2",
+	output = "streets:bigpole_four_side_junction",
 	recipe = {
-		{"","",""},
-		{"streets:bigpole_edge","streets:bigpole",""},
-		{"","",""}
+		{"streets:bigpole_short"},
+		{"streets:bigpole_corner"}
+	}
+})
+
+
+minetest.register_node(":streets:bigpole_cross", {
+	description = "Pole Cross",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	tiles = {"streets_pole.png"},
+	sunlight_propagates = true,
+	groups = {cracky = 1, level = 2, bigpole = 1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.15, -0.5, -0.15, 0.15, 0.5, 0.15},
+			{-0.15, -0.15, -0.5, 0.15, 0.15, 0.5}
+		}
+	},
+	on_place = minetest.rotate_node,
+	digiline = {
+		wire = {
+			rules = {
+				{x= 0, y= 0, z=-1},
+				{x= 0, y= 0, z= 1},
+				{x= 1, y= 0, z= 0},
+				{x=-1, y= 0, z= 0},
+				{x= 0, y=-1, z= 0},
+				{x= 0, y= 1, z= 0},
+			}
+		}
+	}
+})
+
+minetest.register_craft({
+	output = "streets:bigpole_cross",
+	recipe = {
+		{"", "streets:bigpole_short", ""},
+		{"streets:bigpole_short", "", "streets:bigpole_short"},
+		{"", "streets:bigpole_short", ""},
+	}
+})
+
+
+minetest.register_node(":streets:bigpole_five_side_junction", {
+	description = "Pole 5-Side-Junction",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	tiles = {"streets_pole.png"},
+	sunlight_propagates = true,
+	groups = {cracky = 1, level = 2, bigpole = 1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.15, -0.5, -0.15, 0.15, 0.5, 0.15},
+			{-0.15, -0.15, -0.5, 0.15, 0.15, 0.5},
+			{-0.15,-0.15,-0.15,0.5,0.15,0.15}
+		}
+	},
+	on_place = minetest.rotate_node,
+	digiline = {
+		wire = {
+			rules = {
+				{x= 0, y= 0, z=-1},
+				{x= 0, y= 0, z= 1},
+				{x= 1, y= 0, z= 0},
+				{x=-1, y= 0, z= 0},
+				{x= 0, y=-1, z= 0},
+				{x= 0, y= 1, z= 0},
+			}
+		}
+	}
+})
+
+minetest.register_craft({
+	output = "streets:bigpole_fice_side_junction",
+	recipe = {
+		{"streets:bigpole_cross", "streets:bigpole_short"}
+	}
+})
+
+
+minetest.register_node(":streets:bigpole_all_sides", {
+	description = "Pole All Sides",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	tiles = {"streets_pole.png"},
+	sunlight_propagates = true,
+	groups = {cracky = 1, level = 2, bigpole = 1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.15, -0.5, -0.15, 0.15, 0.5, 0.15},
+			{-0.15, -0.15, -0.5, 0.15, 0.15, 0.5},
+			{-0.5, -0.15, -0.15, 0.5, 0.15, 0.15}
+		}
+	},
+	on_place = minetest.rotate_node,
+	digiline = {
+		wire = {
+			rules = {
+				{x= 0, y= 0, z=-1},
+				{x= 0, y= 0, z= 1},
+				{x= 1, y= 0, z= 0},
+				{x=-1, y= 0, z= 0},
+				{x= 0, y=-1, z= 0},
+				{x= 0, y= 1, z= 0},
+			}
+		}
+	}
+})
+
+minetest.register_craft({
+	output = "streets:bigpole_all_sides",
+	recipe = {
+		{"streets:bigpole_short", "streets:bigpole_cross", "streets:bigpole_short"}
 	}
 })
