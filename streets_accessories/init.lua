51bd3c301bd94920bdfb52ea23bcf493e3472243
Thomas--S <Thomas--S@users.noreply.github.com>
2016-06-11 13:16:53 +0200

    Improve delineators.

--- streets_accessories/init.lua
+++ streets_accessories/init.lua
@@ -5,21 +5,22 @@
 ]]
 
 minetest.register_node(":streets:delineator", {
-	tiles = {"streets_delineator_top.png", "streets_delineator_top.png", "streets_delineator_right.png", "streets_delineator_left.png", "streets_delineator_front.png", "streets_delineator_back.png"},
+	description = "Delineator",
+	tiles = {"streets_delineator_top.png", "streets_delineator_top.png", "streets_delineator_right.png", "streets_delineator_left.png", "streets_delineator_back.png", "streets_delineator_front.png"},
 	drawtype = "nodebox",
 	paramtype = "light",
-  paramtype2 = "facedir",
-  groups = {snappy = 2},
-  light_source = 2,
-  sunlight_propagates = true,
+	paramtype2 = "facedir",
+	groups = {snappy = 2},
+	light_source = 2,
+	sunlight_propagates = true,
 	node_box = {
 		type = "fixed",
 		fixed = {
 			{-0.125, -0.5, -0.0625, 0.125, 0.4375, 0.0625}, -- Body
-			{-0.125, 0.4375, -0.0625, 0, 0.5, 0.0625}, -- Top
+			{0, 0.4375, -0.0625, 0.125, 0.5, 0.0625}, -- Top
 		}
 	},
-  selection_box = {
+	selection_box = {
 		type = "fixed",
 		fixed = {
 			{-0.125, -0.5, -0.0625, 0.125, 0.5, 0.0625}, -- Body
