2d182a46e5da2be01185ce5a6bbf3a807a6dbdcf
FirePowi <powi@powi.fr>
2016-10-01 23:07:16 +0200

    Added new lines (dashed, solid and side at 45°) and moreblocks stairplus support and used it on dashed line, solid line and sideline.

--- labels/init.lua
+++ labels/init.lua
@@ -1,13 +1,18 @@
 --[[
 	StreetsMod: All kinds of asphalt with labels
 ]]
+local stairplus = streets.stairplus
+
 minetest.register_node(":streets:asphalt_sideline",{
 	description = streets.S("Asphalt with sideline"),
 	groups = {cracky=3},
 	tiles = {"streets_asphalt.png^streets_asphalt_side.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_sideline")
 minetest.register_alias("streets:asphalt_side","streets:asphalt_sideline")
+minetest.register_alias("streets:asphalt_lside","streets:asphalt_sideline")
+minetest.register_alias("streets:asphalt_left_side","streets:asphalt_sideline")
 minetest.register_craft({
 	output = "streets:asphalt_side 6",
 	recipe = {
@@ -17,12 +22,94 @@ minetest.register_craft({
 	}
 })
 
+minetest.register_node(":streets:asphalt_right_sideline",{
+	description = streets.S("Asphalt with sideline").." 180°",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_side.png^[transformR180","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+stairplus("asphalt_right_sideline")
+minetest.register_alias("streets:asphalt_rside","streets:asphalt_right_sideline")
+minetest.register_craft({
+	output = "streets:asphalt_right_side 6",
+	recipe = {
+		{"streets:asphalt","streets:asphalt","dye:white"},
+		{"streets:asphalt","streets:asphalt","dye:white"},
+		{"streets:asphalt","streets:asphalt","dye:white"}
+	}
+})
+
+minetest.register_node(":streets:asphalt_sideline_45",{
+	description = streets.S("Asphalt with sideline").." 45°",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_side_45.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+minetest.register_alias("streets:asphalt_side_45","streets:asphalt_sideline_45")
+minetest.register_craft({
+	output = "streets:asphalt_side_45 6",
+	recipe = {
+		{"streets:asphalt","streets:asphalt","dye:white"},
+		{"streets:asphalt","dye:white","dye:white"},
+		{"dye:white","dye:white","streets:asphalt"}
+	}
+})
+
+minetest.register_node(":streets:asphalt_sideline_45bis",{
+	description = streets.S("Asphalt with sideline").." 45°bis",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_side_45bis.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+minetest.register_alias("streets:asphalt_side_45b","streets:asphalt_sideline_45bis")
+minetest.register_craft({
+	output = "streets:asphalt_side_45bis 6",
+	recipe = {
+		{"streets:asphalt","streets:asphalt","streets:asphalt"},
+		{"streets:asphalt","streets:asphalt","dye:white"},
+		{"streets:asphalt","dye:white","streets:asphalt"}
+	}
+})
+
+minetest.register_node(":streets:asphalt_sideline_r45",{
+	description = streets.S("Asphalt with sideline").." -45°",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_side_r_45.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+minetest.register_alias("streets:asphalt_side_r45","streets:asphalt_sideline_r45")
+minetest.register_craft({
+	output = "streets:asphalt_side_r45 6",
+	recipe = {
+		{"dye:white","streets:asphalt","streets:asphalt"},
+		{"dye:white","dye:white","streets:asphalt"},
+		{"streets:asphalt","dye:white","dye:white"}
+	}
+})
+
+minetest.register_node(":streets:asphalt_sideline_r45bis",{
+	description = streets.S("Asphalt with sideline").." -45°bis",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_side_r_45bis.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+minetest.register_alias("streets:asphalt_side_r45b","streets:asphalt_sideline_r45bis")
+minetest.register_craft({
+	output = "streets:asphalt_side_r45bis 6",
+	recipe = {
+		{"streets:asphalt","streets:asphalt","streets:asphalt"},
+		{"dye:white","streets:asphalt","streets:asphalt"},
+		{"streets:asphalt","dye:white","streets:asphalt"}
+	}
+})
+
 minetest.register_node(":streets:asphalt_solid_line",{
 	description = streets.S("Asphalt with solid line"),
 	groups = {cracky=3},
 	tiles = {"streets_asphalt.png^streets_asphalt_solid_line.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_solid_line")
 minetest.register_alias("streets:asphalt_middle","streets:asphalt_solid_line")
 minetest.register_craft({
 	output = "streets:asphalt_solid_line 6",
@@ -33,12 +120,64 @@ minetest.register_craft({
 	}
 })
 
+minetest.register_node(":streets:asphalt_solid_line_r45",{
+	description = streets.S("Asphalt with solid line").."0 → 45°",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_solid_line_r45.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+stairplus("asphalt_solid_line")
+minetest.register_alias("streets:asphalt_middle","streets:asphalt_solid_line")
+minetest.register_craft({
+	output = "streets:asphalt_solid_line_r45 6",
+	recipe = {
+		{"streets:asphalt","streets:asphalt","dye:white"},
+		{"streets:asphalt","dye:white","streets:asphalt"},
+		{"streets:asphalt","dye:white","streets:asphalt"}
+	}
+})
+
+minetest.register_node(":streets:asphalt_solid_line_l45",{
+	description = streets.S("Asphalt with solid line").."0 → -45°",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_solid_line_l45.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+stairplus("asphalt_solid_line")
+minetest.register_alias("streets:asphalt_middle","streets:asphalt_solid_line")
+minetest.register_craft({
+	output = "streets:asphalt_solid_line_r45 6",
+	recipe = {
+		{"dye:white","streets:asphalt","streets:asphalt"},
+		{"streets:asphalt","dye:white","streets:asphalt"},
+		{"streets:asphalt","dye:white","streets:asphalt"}
+	}
+})
+
+minetest.register_node(":streets:asphalt_solid_line_45",{
+	description = streets.S("Asphalt with solid line").." 45°",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_solid_line_45.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+minetest.register_alias("streets:asphalt_middle45","streets:asphalt_solid_line_45")
+minetest.register_craft({
+	output = "streets:asphalt_solid_line_45 6",
+	recipe = {
+		{"streets:asphalt","streets:asphalt","dye:white"},
+		{"streets:asphalt","dye:white","streets:asphalt"},
+		{"dye:white","streets:asphalt","streets:asphalt"}
+	}
+})
+
+
 minetest.register_node(":streets:asphalt_dashed_line",{
 	description = streets.S("Asphalt with dashed line"),
 	groups = {cracky=3},
 	tiles = {"streets_asphalt.png^streets_asphalt_dashed_line.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_dashed_line")
 minetest.register_alias("streets:asphalt_middle_dashed","streets:asphalt_dashed_line")
 minetest.register_craft({
 	output = "streets:asphalt_dashed_line 6",
@@ -49,12 +188,79 @@ minetest.register_craft({
 	}
 })
 
+minetest.register_node(":streets:asphalt_dashed_line_r45",{
+	description = streets.S("Asphalt with dashed line").."0 → 45°",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_dashed_line_r45.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+stairplus("asphalt_dashed_line")
+minetest.register_alias("streets:asphalt_middle_dashed","streets:asphalt_dashed_line")
+minetest.register_craft({
+	output = "streets:asphalt_dashed_line 6",
+	recipe = {
+		{"streets:asphalt","streets:asphalt","dye:white"},
+		{"streets:asphalt","streets:asphalt","streets:asphalt"},
+		{"streets:asphalt","dye:white","streets:asphalt"}
+	}
+})
+
+minetest.register_node(":streets:asphalt_dashed_line_l45",{
+	description = streets.S("Asphalt with dashed line").."0 → -45°",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_dashed_line_l45.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+stairplus("asphalt_dashed_line")
+minetest.register_alias("streets:asphalt_middle_dashed","streets:asphalt_dashed_line")
+minetest.register_craft({
+	output = "streets:asphalt_dashed_line 6",
+	recipe = {
+		{"dye:white","streets:asphalt","streets:asphalt"},
+		{"streets:asphalt","streets:asphalt","streets:asphalt"},
+		{"streets:asphalt","dye:white","streets:asphalt"}
+	}
+})
+
+minetest.register_node(":streets:asphalt_dashed_line_45",{
+	description = streets.S("Asphalt with dashed line").."45°" ,
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_dashed_line_45.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+minetest.register_alias("streets:asphalt_middle_dashed","streets:asphalt_dashed_line")
+minetest.register_craft({
+	output = "streets:asphalt_dashed_line_45 6",
+	recipe = {
+		{"streets:asphalt","streets:asphalt","dye:white"},
+		{"streets:asphalt","streets:asphalt","streets:asphalt"},
+		{"dye:white","streets:asphalt","streets:asphalt"}
+	}
+})
+
+minetest.register_node(":streets:asphalt_line45bis",{
+	description = streets.S("Asphalt with line").." 45°bis",
+	groups = {cracky=3},
+	tiles = {"streets_asphalt.png^streets_asphalt_line_45bis.png","streets_asphalt.png"},
+	paramtype2 = "facedir"
+})
+minetest.register_alias("streets:asphalt_middle45bis","streets:asphalt_line45bis")
+minetest.register_craft({
+	output = "streets:asphalt_line45bis 6",
+	recipe = {
+		{"dye:white","streets:asphalt","streets:asphalt"},
+		{"streets:asphalt","streets:asphalt","streets:asphalt"},
+		{"streets:asphalt","streets:asphalt","streets:asphalt"}
+	}
+})
+
 minetest.register_node(":streets:asphalt_outer_edge",{
 	description = streets.S("Asphalt with sideline (edge)"),
 	groups = {cracky=3},
 	tiles = {"streets_asphalt.png^streets_asphalt_outer_edge.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_outer_edge")
 -- no minetest.register_alias() needed
 minetest.register_craft({
 	output = "streets:asphalt_outer_edge 4",
@@ -71,6 +277,7 @@ minetest.register_node(":streets:asphalt_parking",{
 	tiles = {"streets_asphalt.png^streets_parking.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_parking")
 -- no minetest.register_alias() needed
 minetest.register_craft({
 	output = "streets:asphalt_parking 4",
@@ -87,6 +294,7 @@ minetest.register_node(":streets:asphalt_arrow_straight",{
 	tiles = {"streets_asphalt.png^streets_arrow_straight.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_arrow_straight")
 -- no minetest.register_alias() needed
 minetest.register_craft({
 	output = "streets:asphalt_arrow_straight 4",
@@ -103,6 +311,7 @@ minetest.register_node(":streets:asphalt_arrow_left",{
 	tiles = {"streets_asphalt.png^streets_arrow_left.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_arrow_left")
 -- no minetest.register_alias() needed
 minetest.register_craft({
 	output = "streets:asphalt_arrow_left 4",
@@ -119,6 +328,7 @@ minetest.register_node(":streets:asphalt_arrow_right",{
 	tiles = {"streets_asphalt.png^streets_arrow_right.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_arrow_right")
 -- no minetest.register_alias() needed
 minetest.register_craft({
 	output = "streets:asphalt_arrow_right 4",
@@ -135,6 +345,7 @@ minetest.register_node(":streets:asphalt_arrow_straight_left",{
 	tiles = {"streets_asphalt.png^streets_arrow_straight_left.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_arrow_straight_left")
 -- no minetest.register_alias() needed
 minetest.register_craft({
 	output = "streets:asphalt_arrow_straight_left 4",
@@ -151,6 +362,7 @@ minetest.register_node(":streets:asphalt_arrow_straight_right",{
 	tiles = {"streets_asphalt.png^streets_arrow_straight_right.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_arrow_straight_right")
 -- no minetest.register_alias() needed
 minetest.register_craft({
 	output = "streets:asphalt_arrow_straight_right 4",
@@ -167,6 +379,7 @@ minetest.register_node(":streets:asphalt_arrow_alldirs",{
 	tiles = {"streets_asphalt.png^streets_arrow_alldirs.png","streets_asphalt.png"},
 	paramtype2 = "facedir"
 })
+stairplus("asphalt_arrow_alldirs")
 -- no minetest.register_alias() needed
 minetest.register_craft({
 	output = "streets:asphalt_arrow_alldirs 4",
