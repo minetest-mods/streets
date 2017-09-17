e750bec028e8feb5313ee83f3adf2187be417159
Thomas--S <info@thomas-stangl.de>
2017-09-17 19:32:03 +0200

    Add some EU signs

--- streets_signs/eu/euother.lua
+++ streets_signs/eu/euother.lua
@@ -1,5 +1,6 @@
 local euother = {
-	{ "guideboard", "Guide Board", { white = 2, red = 2 } },
+	{ "guideboard_left", "Guide Board (Left)", { white = 2, red = 2 } },
+	{ "guideboard_right", "Guide Board (Right)", { white = 2, red = 2 } },
 	{ "bendright", "Guide Sign in Bends", { white = 2, red = 2 } },
 	{ "bendleft", "Guide Sign in Bends", { white = 2, red = 2 } },
 	{ "pedestriansleft", "Pedestrians to the Left", { white = 2, black = 1 } },
@@ -35,6 +36,8 @@ local euother_big = {
 	{ "additionallane", "Additional Lane", { white = 3, black = 1 } },
 	{ "mergelanes", "Merge Lanes", { white = 3, black = 1 } },
 	{ "laneshift", "Lane Shift", { white = 3, black = 1 } },
+	{ "transfertoothercarriageway1", "Transfer to other Carriageway (1 Lane)", { white = 3, black = 1 } },
+	{ "transfertoothercarriageway2", "Transfer to other Carriageway (2 Lanes)", { white = 3, black = 1 } },
 }
 
 for k, v in pairs(euother_big) do
@@ -52,3 +55,7 @@ for k, v in pairs(euother_big) do
 		inventory_image = "streets_sign_eu_" .. v[1] .. "_inv.png",
 	})
 end
+
+minetest.register_alias("streets:sign_eu_guideboard", "streets:sign_eu_guideboard_right")
+minetest.register_alias("streets:sign_eu_guideboard_center", "streets:sign_eu_guideboard_right_center")
+minetest.register_alias("streets:sign_eu_guideboard_polemount", "streets:sign_eu_guideboard_right_polemount")
